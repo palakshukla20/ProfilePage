29f13b74d277353eeb6b9cbc3fb2c194304d782d
palakshukla20 <70757919+palakshukla20@users.noreply.github.com>
2020-10-27 17:26:49 +0530

    Assignment

--- index.js
+++ index.js
@@ -1,6 +1,5 @@
 $(document).ready(function() {
-  $("#navbar").load("index.html #header");
-  $("#body-nav").load("index.html #body-right-nav");
+
 })
 
 function isLogout() {
@@ -10,27 +9,31 @@ function isLogout() {
 }
 
 function load(e, id) {
-  $(".profile-inner-nav-list li").removeClass("active") 
-  console.log(('#'+ id))
-  $('#'+ id).addClass("active"); 
+  if (localStorage.getItem("email") == null) { 
+    window.location.href = '/login.html';
+  }
   var object = {}
   var getUsersData = JSON.parse(localStorage.getItem("Users"))
   var currentUser = localStorage.getItem("email")
   
   $.each(getUsersData, function(key, val) {
-    console.log(typeof val)
     if (currentUser === val.email) {  
-      console.log(val)
       var result = []
       result.push(val)
       object["result"] = result;
+      object["post"] = val.post;
+      object["friend"] = val.friend;
+      object["photo"] = val.photo;
       var template = $('#template').html();
       var text = Mustache.render(template, object);
       $("#load-template").html(text);
+      $("#navbar").load("index.html #header");
+      $("#body-nav").load("index.html #body-right-nav");
+      $("#profile-card").load("index.html .profile-card")
+      $('#' + id).addClass('active').siblings().removeClass('active');
     }
   })
 }
-
 
 $('#submitForm').on('click', function() {
   if ($("#password1").val() === $("#password2").val()) {
@@ -39,8 +42,12 @@ $('#submitForm').on('click', function() {
     email: $("#emailLogin").val(),
     password: $("#password1").val(),
     birthdate: $("#date").val(),
-    location: $("#location").val()};
-
+    location: $("#location").val(),
+    defaultImage: "default-profile-picture.jpg",
+    post: [],
+    friend: [],
+    allfriend: [],
+    photo: [] };
     registerData.push(userData);
     console.log(registerData)
     localStorage.setItem('Users', JSON.stringify(registerData));
@@ -62,32 +69,165 @@ $('#login').on('click', function() {
   })
 })
 
-function updateData(){
+function updateData() {
   var currentUser = localStorage.getItem('email');
   var currentUserData = JSON.parse(localStorage.getItem('Users')); 
-  $.each(currentUserData, function(key, val) {
-    if (currentUser === val.email) {
-      console.log(val.name)
-      val.name = ($("#name").val().length )== 0 ? val.name = val.name :  val.name = val.name = $("#name").val();
-      console.log(val.name)
+  for (var i = 0; i < currentUserData.length; i++) {
+    if(currentUser === currentUserData[i].email) { 
+      var genderData = $("input:radio[name=gender]:checked").val();
+      var statusData = $("input:radio[name=status]:checked").val()
+      currentUserData[i].name = $("#name").val();
+      currentUserData[i].title = $("#title").val();
+      currentUserData[i].date =  ($("#date").val()).toLocaleString(); 
+      currentUserData[i].location = $("#location").val(); 
+      currentUserData[i].gender = genderData == null ? currentUserData[i].gender =  currentUserData[i].gender : currentUserData[i].gender = genderData ; 
+      currentUserData[i].status = statusData== null ? currentUserData[i].status =  currentUserData[i].status : currentUserData[i].status = statusData;
+      currentUserData[i].occupation = $("#occupation").val();
+      currentUserData[i].job = $("#job").val();
+      currentUserData[i].skill = $("#skill").val();
+      var imagePath = $("#profile-image").val()
+      imagePath = imagePath.replace(/^.*\\/, "")
+      currentUserData[i].profileImage = 
+      $("#profile-image").val().length == 0 ? currentUserData[i].profileImage = currentUserData[i].profileImage : 
+      currentUserData[i].profileImage = imagePath;
+      window.location.reload();  
+      break; 
     }
-  })
+  }
+  localStorage.setItem("Users", JSON.stringify(currentUserData)); 
 }
 
-function addFriend() {
+function getUserPrevInfo() {
+  var currentUser = localStorage.getItem('email');
+  var currentUserData = JSON.parse(localStorage.getItem('Users')); 
+  for (var i = 0; i < currentUserData.length; i++) {
+    if(currentUser === currentUserData[i].email) {   
+      $("#name").val(currentUserData[i].name); 
+      $("#title").val(currentUserData[i].title); 
+      $("#date").val(currentUserData[i].date); 
+      $("#location").val(currentUserData[i].location); 
+      $("#gender").val(currentUserData[i].gender); 
+      $("#status").val(currentUserData[i].gender); 
+      $('#occupation').val(currentUserData[i].occupation);
+      $('#job').val(currentUserData[i].job); 
+      $('#skill').val(currentUserData[i].skill);
+      break; 
+    }
+  }
+}
+
+function addPost() {
+  
+    var currentUser = localStorage.getItem('email'); 
+    var currentUserData = JSON.parse(localStorage.getItem('Users')); 
+    for (var i = 0; i < currentUserData.length; i++) {
+      if(currentUser === currentUserData[i].email) { 
+        var object = {};
+        var imagePath = $("#post-image").val();
+        imagePath = imagePath.replace(/^.*\\/, "");
+        object["postImage"] = imagePath;
+        currentUserData[i].photo = imagePath
+        object["postText"] = $("#post-text").val();
+        object["date"] = new Date().toLocaleString()
+        currentUserData[i].post.unshift(object);
+        console.log(currentUserData[i].post)
+        window.location.reload(); 
+        break;
+      }
+    }
+    localStorage.setItem("Users", JSON.stringify(currentUserData)); 
+    countActivity()
+}
+
+function getFriendDetail(getNewfriend) {
+  var currentUserData = JSON.parse(localStorage.getItem('Users')); 
+  for (var i = 0; i < currentUserData.length; i++) {
+    if(getNewfriend === currentUserData[i].email) {   
+     object = {}
+     console.log(currentUserData[i].name)
+     console.log(currentUserData[i].job)
+     object["friendName"] = currentUserData[i].name;
+     object["friendJob"] = currentUserData[i].job;
+     object["friendImage"] = currentUserData[i].profileImage;
+     return object
+    }
+  }
+}
+
+function showfriend() {
+  $('.follow-card').css('display', 'block')
+  var friend = [];
+  var obj = {};
   var currentUser = localStorage.getItem('email');
   var currentUserData = JSON.parse(localStorage.getItem('Users')); 
   $.each(currentUserData, function(key, val) {
     if (currentUser !== val.email) {
-      a = "<a>`{{val.name}}` </a>"
-      $('follow-card').append(a)
+      object = {};
+      object["name"] = val.name;
+      object["title"] = val.title;
+      object["email"] = val.email;
+      friend.unshift(object)
     }
+  obj["add-friend"] = friend
+  var template = $("#friend-template").html();
+  text =  Mustache.render(template, obj);
+  $(".add-friend").html(text)
   })
 }
 
+
+function addNewFriend(e) {
+  var getNewfriend = $(e).attr("id");
+  var currentUser = localStorage.getItem("email");
+  var currentUserData = JSON.parse(localStorage.getItem("Users")); 
+  for (var i = 0; i < currentUserData.length; i++) {
+    if(currentUser === currentUserData[i].email) {   
+      if (currentUserData[i].allfriend != null && currentUserData[i].allfriend.includes(getNewfriend)) {
+        alert("Already you Both are Friend");
+      }  else {
+        console.log(getNewfriend,  currentUserData[i])
+        currentUserData[i].allfriend.push(getNewfriend);
+        currentUserData[i].friend.push(getFriendDetail(getNewfriend));
+        currentUserData[i].following = (currentUserData[i].friend).length
+        alert("congratulations Now You Both are Friend");
+      }
+    }
+  }
+  console.log(currentUserData)
+  localStorage.setItem("Users", JSON.stringify(currentUserData)); 
+  window.load()
+}
+
+function countActivity () {
+  var currentUser = localStorage.getItem("email");
+  var currentUserData = JSON.parse(localStorage.getItem("Users")); 
+  for (var i = 0; i < currentUserData.length; i++) {
+    if(currentUser === currentUserData[i].email) {   
+      currentUserData[i].activity = (currentUserData[i].post).length;
+    }
+  }
+  localStorage.setItem("Users", JSON.stringify(currentUserData)); 
+}
+
+function addPhoto() {
+  var currentUser = localStorage.getItem("email");
+  var currentUserData = JSON.parse(localStorage.getItem("Users")); 
+  for (var i = 0; i < currentUserData.length; i++) {
+    if(currentUser === currentUserData[i].email) {   
+      var imagePath = $("#photo").val();
+      object = {}
+      object[photoUrl] = imagePath
+      imagePath = imagePath.replace(/^.*\\/, "");
+      currentUserData[i].photo.push(imagePath);
+    }
+  }
+  localStorage.setItem("Users", JSON.stringify(currentUserData)); 
+  window.load()
+}
+
 function getCurrentUser() {
-  var currentUser = localStorage.getItem('email');
-  var currentUserData = JSON.parse(localStorage.getItem('Users')); 
+  var currentUser = localStorage.getItem("email");
+  var currentUserData = JSON.parse(localStorage.getItem("Users")); 
   $.each(currentUserData, function(key, val) {
     if (currentUser === val.email) {
       return (val, currentUser)
